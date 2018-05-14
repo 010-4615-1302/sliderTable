3c965ece749cce720517078d5142853af83d85bc
D7703_29 <d7703_29@d7703-29ui-imac.local>
2018-05-14 15:58:00 +0900

    create

--- sliderTable/ViewController.swift
+++ sliderTable/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
+    @IBOutlet var mySlider: UISlider!
+    
+    @IBOutlet var myTableView: UITableView!
+     @IBOutlet weak var text: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTableView.dataSource = self
+        myTableView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliderMoved(_ sender: Any) {
+        text.text = String(Int(mySlider.value))
+        let a = IndexPath(row: Int(mySlider.value), section:0)
+        //row 값을 현재 slider의 값으로 변경
+        myTableView.scrollToRow(at: a, at: UITableViewScrollPosition.top, animated: true)
+        //tableview의 row값을 현재 a 값이랑 같은 위치로 이동
+       
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 101;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "RE"
+        let cell = myTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        cell.textLabel?.text = String(indexPath.row)
+        return  cell
+    }
+    
 }
 
